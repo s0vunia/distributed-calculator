5317694901c6993cb4e9727a0e88a9533abaca81
Dmitriy <burindima324@gmail.com>
2024-04-19 20:15:57 +0300

    add reconnecting to integrations tests

--- test-integration/gRPCorchestrator/main_test.go
+++ test-integration/gRPCorchestrator/main_test.go
@@ -1,7 +1,12 @@
 package gRPCorchestrator
 
 import (
+	"context"
 	"fmt"
+	protos "github.com/s0vunia/protos/gen/go/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"os"
 	"os/exec"
 	"testing"
@@ -13,10 +18,44 @@ type exprRes struct {
 	res     float32
 }
 
+// ReconnectDelay - начальная задержка перед попыткой переподключения.
+const ReconnectDelay = 10 * time.Second
+
+// MaxReconnectDelay - максимальная задержка перед попыткой переподключения.
+const MaxReconnectDelay = 30 * time.Second
+
+// ReconnectBackoff - коэффициент увеличения задержки.
+const ReconnectBackoff = 2
+
 func TestMain(m *testing.M) {
 	agents := 3
 	cmd := exec.Command("make", "up", fmt.Sprintf("AGENTS=%d", agents))
 	cmd.Dir = "../../"
 	cmd.Run()
+
+	conn, _ := grpc.Dial("localhost:44044", grpc.WithInsecure())
+
+	// test connection
+	authClient := protos.NewAuthClient(conn)
+	_, err := authClient.Register(context.Background(), &protos.RegisterRequest{
+		Login:    "",
+		Password: "",
+	})
+	st, ok := status.FromError(err)
+	delay := ReconnectDelay
+	for ok && st.Code() == codes.Unavailable {
+		fmt.Printf("Reconnect...\n")
+		time.Sleep(delay)
+		delay *= ReconnectBackoff
+		if delay > MaxReconnectDelay {
+			delay = MaxReconnectDelay
+		}
+		_, err := authClient.Register(context.Background(), &protos.RegisterRequest{
+			Login:    "",
+			Password: "",
+		})
+		st, ok = status.FromError(err)
+	}
+
 	os.Exit(m.Run())
 }
