99c2e88673d225d429af3ebb905ff52bf8d12923
Dmitriy <burindima324@gmail.com>
2024-04-20 18:05:09 +0300

    fix expression migrations

--- data/migrations/expressions.sql
+++ data/migrations/expressions.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS expressions
 (
     id UUID PRIMARY KEY,
-    user_id VARCHAR(255) UNIQUE NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
     idempotency_key VARCHAR(255) NOT NULL,
     value TEXT NOT NULL,
     state VARCHAR(50) NOT NULL,
